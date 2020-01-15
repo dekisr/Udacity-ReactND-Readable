55835510b9d4bf7df812084311d5ca99f7a362ab
Hideki J <aledinai@gmail.com>
2020-01-15 16:17:53 -0300

    Update Footer styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import StyledFooter from './styles'
+import FooterMark from '../../assets/FooterMark.png'
 
 const Footer = () => {
   return (
     <StyledFooter>
+      <p>
+        <img src={FooterMark} alt="GitHub Mark" />
+      </p>
       <h2>About</h2>
       <p>
-        This project was made just for study purposes. I tried to put all my
+        {`This project was made just for study purposes. I tried to put all my
         passion to build a bunch of functionalities from scratch. You can find
-        more about and all the code at my{' '}
+        more about and all the code at `}
         <a
           href="https://github.com/dekisr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          GitHub
+          github.com/dekisr
         </a>
         . I'll appreciate any feedback.
       </p>
--- client/src/components/Footer/styles.js
+++ client/src/components/Footer/styles.js
@@ -1,17 +1,38 @@
 import styled from 'styled-components'
+import { colors } from '../../utils/globalStyles'
 
 const StyledFooter = styled.footer`
   width: 100%;
   margin: 1rem auto 0 auto;
+  margin-bottom: 2.5rem;
   padding: 0.5rem;
-  font-size: 1.2rem;
-  border-top: 0.3rem solid snow;
-  color: snow;
-  background-color: salmon;
+  font-size: 1rem;
+  text-align: center;
+  line-height: 1.3;
+  border-top: 0.3rem solid ${colors.brown.five};
+  color: ${colors.brown.five};
+  background-color: none;
+  & h2 {
+    margin-bottom: 0.5rem;
+  }
+  & a {
+    padding: 0.125rem;
+    text-decoration: none;
+    color: snow;
+    background: none;
+  }
+  & a:hover {
+    cursor: pointer;
+    color: ${colors.brown.one};
+    background-color: ${colors.brown.five};
+  }
+  & img {
+    padding: 0.5rem;
+    vertical-align: middle;
+  }
   @media (min-width: 601px) {
     width: calc(100% - 1rem);
     max-width: calc(1120px - 1rem);
-    margin-bottom: 0.5rem;
   }
 `
 
