095a6dc640df5551273286bacaa05084e6d34692
Hideki J <aledinai@gmail.com>
2020-01-29 12:54:04 -0300

    Update server messages

--- server/server.js
+++ server/server.js
@@ -56,97 +56,9 @@ app.use(cors(corsOptions))
 app.get('/', (req, res) => {
   const help = `
   <pre>
-    Welcome to the Udacity Readable API!
-
-    Use an Authorization header to work with your own data:
-
-    fetch(url, { headers: { 'Authorization': 'whatever-you-want' }})
-
-    The following endpoints are available:
-
-    GET /categories
-      USAGE:
-        Get all of the categories available for the app. List is found in categories.js.
-        Feel free to extend this list as you desire.
-
-    GET /:category/posts
-      USAGE:
-        Get all of the posts for a particular category
-
-    GET /posts
-      USAGE:
-        Get all of the posts. Useful for the main page when no category is selected.
-
-    POST /posts
-      USAGE:
-        Add a new post
-
-      PARAMS:
-        id - UUID should be fine, but any unique id will work
-        timestamp - timestamp in whatever format you like, you can use Date.now() if you like
-        title - String
-        body - String
-        author - String
-        category: Any of the categories listed in categories.js. Feel free to extend this list as you desire.
-
-    GET /posts/:id
-      USAGE:
-        Get the details of a single post
-
-    POST /posts/:id
-      USAGE:
-        Used for voting on a post
-      PARAMS:
-        option - String: Either "upVote" or "downVote"
-
-    PUT /posts/:id
-      USAGE:
-        Edit the details of an existing post
-      PARAMS:
-        title - String
-        body - String
-
-    DELETE /posts/:id
-      USAGE:
-        Sets the deleted flag for a post to 'true'.
-        Sets the parentDeleted flag for all child comments to 'true'.
-
-    GET /posts/:id/comments
-      USAGE:
-        Get all the comments for a single post
-
-    POST /comments
-      USAGE:
-        Add a comment to a post
-
-      PARAMS:
-        id: Any unique ID. As with posts, UUID is probably the best here.
-        timestamp: timestamp. Get this however you want.
-        body: String
-        author: String
-        parentId: Should match a post id in the database.
-
-    GET /comments/:id
-      USAGE:
-        Get the details for a single comment
-
-    POST /comments/:id
-      USAGE:
-        Used for voting on a comment.
-      PARAMS:
-        option - String: Either "upVote" or "downVote"
-
-    PUT /comments/:id
-      USAGE:
-        Edit the details of an existing comment
-
-      PARAMS:
-        timestamp: timestamp. Get this however you want.
-        body: String
-
-    DELETE /comments/:id
-      USAGE:
-        Sets a comment's deleted flag to 'true'
+    Readable Project Server API.
+    This is a project for study purposes.
+    Find more information at github.com/dekisr
  </pre>
   `
 
@@ -162,7 +74,7 @@ app.use((req, res, next) => {
   } else {
     res.status(403).send({
       error:
-        'Please provide an Authorization header to identify yourself (can be whatever you want)'
+        'Please provide an Authorization header to identify yourself.'
     })
   }
 })
@@ -173,7 +85,7 @@ app.get('/categories', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting categories.'
       })
     }
   )
@@ -185,7 +97,7 @@ app.get('/:category/posts', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting posts by category.'
       })
     }
   )
@@ -198,7 +110,7 @@ app.get('/posts/reset', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error reseting posts.'
       })
     }
   )
@@ -210,7 +122,7 @@ app.get('/posts', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting posts.'
       })
     }
   )
@@ -222,7 +134,7 @@ app.post('/posts', bodyParser.json(), (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error sending the post.'
       })
     }
   )
@@ -234,7 +146,7 @@ app.get('/posts/:id', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting the post.'
       })
     }
   )
@@ -249,7 +161,7 @@ app.delete('/posts/:id', (req, res) => {
       (error) => {
         console.error(error)
         res.status(500).send({
-          error: 'There was an error.'
+          error: 'There was an error deleting the post.'
         })
       }
     )
@@ -263,7 +175,7 @@ app.post('/posts/:id', bodyParser.json(), (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error sending the post.'
       })
     }
   )
@@ -275,7 +187,7 @@ app.put('/posts/:id', bodyParser.json(), (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error updating the post.'
       })
     }
   )
@@ -287,7 +199,7 @@ app.get('/posts/:id/comments', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting comments.'
       })
     }
   )
@@ -300,7 +212,7 @@ app.get('/comments/reset', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error reseting comments.'
       })
     }
   )
@@ -313,7 +225,7 @@ app.get('/comments', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting comments.'
       })
     }
   )
@@ -325,7 +237,7 @@ app.get('/comments/:id', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error requesting the comment.'
       })
     }
   )
@@ -337,7 +249,7 @@ app.put('/comments/:id', bodyParser.json(), (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error updating the comment.'
       })
     }
   )
@@ -349,7 +261,7 @@ app.post('/comments', bodyParser.json(), (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error sending the comment.'
       })
     }
   )
@@ -363,7 +275,7 @@ app.post('/comments/:id', bodyParser.json(), (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error sending the comment.'
       })
     }
   )
@@ -375,7 +287,7 @@ app.delete('/comments/:id', (req, res) => {
     (error) => {
       console.error(error)
       res.status(500).send({
-        error: 'There was an error.'
+        error: 'There was an error deleting the comment.'
       })
     }
   )
